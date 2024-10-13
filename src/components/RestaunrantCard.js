39ddeba7c4787f962347b5261cd402c62149327f
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2024-10-13 15:23:41 +0530

    Improvement

--- src/components/RestaunrantCard.js
+++ src/components/RestaunrantCard.js
@@ -16,7 +16,7 @@ const RestaunrantCard = (props) => {
         <img
           className="res-logo"
           alt="res-logo"
-          src={CDN_URL+cloudinaryImageId}
+          src={CDN_URL + cloudinaryImageId}
         />
         <h3>{name}</h3>
         <h4>{cuisines.join(",")}</h4>
