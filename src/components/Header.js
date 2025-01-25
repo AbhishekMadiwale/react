2e15588bbeffe75c64d00c47fcd58b6f6cf172e9
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2025-01-25 16:26:56 +0530

    Test cases for header component implemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
 
   // Subscribing the store with Selector Hook
   const cartItems = useSelector((store) => store.cart.items)
-  console.log(cartItems)
+  // console.log(cartItems)
 
   return (
     <div className="flex justify-between shadow-lg sm:bg-yellow-500 md:bg-green-500 lg:bg-pink-100">
---
+++ src/components/__tests__/header.test.js
@@ -0,0 +1,56 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Header from "../Header";
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+test("should render header component with a login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  // const loginButton = screen.getByRole("button");
+
+  // Another method to get the button (through the text and it is not a good way to get a button)
+
+  const loginButton = screen.getByText("Login");
+
+  expect(loginButton).toBeInTheDocument();
+});
+
+test("render header component with 0 items", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const cardItems = screen.getByText("Cart = 0 items");
+
+  expect(cardItems).toBeInTheDocument();
+});
+
+test("Should change login button to logout on click", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginBtn = screen.getByRole("button", { name: "Login" });
+
+  fireEvent.click(loginBtn);
+
+  const logoutBtn = screen.getByRole("button", { name: "Logout" });
+
+  expect(logoutBtn).toBeInTheDocument();
+});
